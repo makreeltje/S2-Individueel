f9ad2b64ab591e75f52aef1482abc139776bb281
Rick Meels <rick.meels@gmail.com>
2019-09-18 14:56:12 +0200

    Created prime factor

---
+++ Puzzels - 3/Puzzels - 3/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puzzels___3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            long a, b;
+            a = 600851475143;
+            for (b = 2; a > 1; b++)
+                if (a % b == 0)
+                {
+                    int x = 0;
+                    while (a % b == 0)
+                    {
+                        a /= b;
+                        x++;
+                    }
+
+                    listBox1.Items.Add(b);
+                }
+        }
+    }
+}
