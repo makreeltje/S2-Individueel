d722af21d40f624790b036eb904445800b89a007
Rick Meels <rick.meels@gmail.com>
2019-09-18 14:31:44 +0200

    Created app

---
+++ Puzzels - 1/Puzzels - 1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puzzels___1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            for (int i = 3; i < 1000; i = i + 3)
+            {
+                listBox1.Items.Add(i);
+            }
+
+            for (int i = 5; i < 1000; i = i + 5)
+            {
+                listBox1.Items.Add(i);
+            }
+
+            int j = 0;
+
+            foreach (int item in listBox1.Items)
+            {
+                j = j + item;
+            }
+
+            label1.Text = j.ToString();
+
+        }
+    }
+}
