de1bd706d2c5d95cb2980f947e15db65b6a3d2bd
Rick Meels <rick.meels@gmail.com>
2019-09-19 15:03:14 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Casus - Circustrein/Casus - CircustreinTests/WagonTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Casus___Circustrein;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casus___Circustrein.Tests
+{
+    [TestClass()]
+    public class WagonTests
+    {
+        [TestMethod()]
+        public void TryAddAnimalTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
