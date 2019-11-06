20d7befa864ee5110bcf93a62143aad4ad91e7c6
Rick Meels <rick.meels@gmail.com>
2019-11-06 14:12:18 +0100

    First step Casus - ContainerVervoer
    - Added application Casus - Containervervoer
    - Added 3 classes with props (Place, Ship, Container)

---
+++ Casus - Containervervoer/Casus - Containervervoer/Container.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Proxies;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casus___Containervervoer
+{
+    class Container
+    {
+        public enum category
+        {
+            valuable,
+            cooled,
+            standard
+        }
+
+        public category Categories { get; set; }
+        public int Weight { get; set; }
+    }
+}
---
+++ Casus - Containervervoer/Casus - Containervervoer/Ship.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casus___Containervervoer
+{
+    class Ship
+    {
+        public int MaxWeight { get; set; }
+        public int MinWeight { get; set; }
+        public int Balance { get; set; }
+        public int WeightLeft { get; set; }
+        public int WeightRight { get; set; }
+
+        public Ship()
+        {
+            
+        }
+    }
+    
+    
+}
